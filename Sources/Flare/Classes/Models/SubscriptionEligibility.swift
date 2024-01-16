d4fe9819e197bf96f8af636a6a4c0f1fb453f8a4
Nikita Vasilev <nv3212@gmail.com>
2024-01-16 13:10:01 +0100

    Add comments to the `SubscriptionEligibility` enumeration

--- Sources/Flare/Classes/Models/SubscriptionEligibility.swift
+++ Sources/Flare/Classes/Models/SubscriptionEligibility.swift
@@ -5,8 +5,14 @@
 
 import Foundation
 
+// Enumeration defining the eligibility status for a subscription
 public enum SubscriptionEligibility: Int, Sendable {
+    // Represents that the subscription is eligible for an offer
     case eligible
+
+    // Represents that the subscription is not eligible for an offer
     case nonEligible
+
+    // Represents that there is no offer available for the subscription
     case noOffer
 }
