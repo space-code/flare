7f0859fd68c1eb40101cb5a3ecbe91f74d219ef7
Nikita Vasilev <nv3212@gmail.com>
2025-04-05 22:41:35 +0400

    Implement the `isEligibleForIntroOffer` property

--- Sources/Flare/Classes/Models/Internal/Protocols/ISubscriptionInfo.swift
+++ Sources/Flare/Classes/Models/Internal/Protocols/ISubscriptionInfo.swift
@@ -5,6 +5,17 @@
 
 import Foundation
 
+/// A protocol that defines the interface for retrieving subscription-related information.
 protocol ISubscriptionInfo {
+    /// An asynchronous computed property that returns an array of the user's current subscription statuses.
+    ///
+    /// - Returns: An array of `SubscriptionInfoStatus` objects representing various subscription states.
+    ///
+    /// - Throws: An error if fetching the subscription status fails.
     var subscriptionStatus: [SubscriptionInfoStatus] { get async throws }
+
+    /// An asynchronous computed property indicating whether the user is eligible for an introductory offer.
+    ///
+    /// - Returns: A `SubscriptionEligibility` value representing the user's eligibility status.
+    var isEligibleForIntroOffer: SubscriptionEligibility { get async }
 }
